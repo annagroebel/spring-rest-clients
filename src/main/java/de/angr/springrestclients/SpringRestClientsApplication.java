01ff47b01765b966b96149d136c2544d38567fe2
Anna Gröbel <an.groebel@outlook.de>
2025-06-29 19:52:59 +0200

    feat: Initial commit: Setup template spring boot project

---
+++ src/main/java/de/angr/springrestclients/SpringRestClientsApplication.java
@@ -0,0 +1,13 @@
+package de.angr.springrestclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestClientsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestClientsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/angr/springrestclients/SpringRestClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.angr.springrestclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestClientsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
